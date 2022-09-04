073295db4ed98ff88a58b7e06e24728b8a09e836
Lucas <lucs.25@hotmail.com>
2022-09-04 11:39:26 -0300

    Envio dos Arquivos

---
+++ Tabuada.js
@@ -0,0 +1,19 @@
+function tabuada() {
+    let mul = document.getElementById("txi")
+    let res = document.getElementById("res")
+    if (mul.value.length == 0) {
+        window.alert("Favor inserir um número")
+    } else {
+        let mt = Number(mul.value)
+        let fat = 1
+        res.innerHTML = ''
+        do {
+            let item = document.createElement('option')
+            item.text = `${mt}x${fat} = ${mt*fat}`
+            res.appendChild(item)
+            mt * fat
+            fat ++
+        } while (fat <= 10) {
+        }
+    }
+}
