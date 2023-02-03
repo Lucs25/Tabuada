41522657493870997d5e602c4be814ffa0e37419
Lucas <lucs.25@hotmail.com>
2023-02-03 18:42:47 -0300

    novas atualizações

--- Tabuada.js
+++ Tabuada.js
@@ -1,19 +1,18 @@
 function tabuada() {
     let mul = document.getElementById("txi")
     let res = document.getElementById("res")
-    if (mul.value.length == 0) {
+    if (mul.value.length == 0)
         window.alert("Favor inserir um número")
-    } else {
-        let mt = Number(mul.value)
+    else {
+        let mt = (+mul.value)
         let fat = 1
         res.innerHTML = ''
         do {
             let item = document.createElement('option')
-            item.text = `${mt}x${fat} = ${mt*fat}`
+            item.text = `${mt}x${fat} = ${mt * fat}`
             res.appendChild(item)
-            mt * fat
             fat ++
         } while (fat <= 10) {
-        }
+            }
     }
 }
